51f7dc259942321d75f1c53153955e247fe93d58
zalansari <zainabkalansari8679@gmail.com>
2024-10-12 19:19:36 +0300

    k

--- handlers/handler.go
+++ handlers/handler.go
@@ -5,12 +5,10 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-
 	"golang.org/x/crypto/bcrypt"
 )
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
-
 // renderTemplate helper function
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", data)
@@ -18,25 +16,20 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 		http.Error(w, "Unable to load template", http.StatusInternalServerError)
 	}
 }
-
 // BaseHandler serves pages with the base layout (base.html)
 func BaseHandler(w http.ResponseWriter, r *http.Request, templateName string, data interface{}) {
 	userID, isLoggedIn := GetUserIDFromSession(r)
 	templateName = "base"
-
 	pageData := make(map[string]interface{})
-
 	// Common data across all templates using base.html
 	pageData["IsLoggedIn"] = isLoggedIn
 	pageData["UserID"] = userID
-
 	// Render the template with base.html as the layout
 	err := templates.ExecuteTemplate(w, templateName+".html", pageData)
 	if err != nil {
 		http.Error(w, "Unable to render page", http.StatusInternalServerError)
 	}
 }
-
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	userID, isLoggedIn := GetUserIDFromSession(r)
 	posts, err := models.GetAllPosts()
@@ -57,7 +50,6 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	BaseHandler(w, r, "home", data)
 }
-
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		data := struct {
@@ -71,13 +63,11 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		email := r.FormValue("email")
 		username := r.FormValue("username")
 		password := r.FormValue("password")
-
 		// Validate inputs
 		if email == "" || username == "" || password == "" {
 			http.Error(w, "All fields are required", http.StatusBadRequest)
 			return
 		}
-
 		// Hash the password
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		if err != nil {
@@ -85,14 +75,12 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
-
 		// Create a new user object
 		newUser := models.User{
 			Email:    email,
 			Username: username,
 			Password: string(hashedPassword),
 		}
-
 		// Save the user to the database
 		err = models.CreateUser(newUser)
 		if err != nil {
@@ -105,7 +93,6 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		user, err := models.GetUserByEmail(email)
-
 		if err != nil {
 			http.Error(w, "Invalid login", http.StatusUnauthorized)
 			return
@@ -121,7 +108,6 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	} else if r.Method == http.MethodPost {
 		Email_UserName := r.FormValue("email")
 		password := r.FormValue("password")
-
 		user, err := models.GetUserByEmail(Email_UserName)
 		if err != nil {
 			user, err = models.GetUserByUserName(Email_UserName)
@@ -130,19 +116,16 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Invalid login", http.StatusUnauthorized)
 			return
 		}
-
 		CreateSession(w, user.Username)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
-
 func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 	userID, loggedIn := GetUserIDFromSession(r)
 	if !loggedIn {
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
 	}
-
 	if r.Method == http.MethodGet {
 		RenderTemplate(w, "create_post", nil)
 	} else if r.Method == http.MethodPost {
@@ -156,7 +139,6 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
-
 func ViewPostHandler(w http.ResponseWriter, r *http.Request) {
 	postID := r.URL.Query().Get("id")
 	post, err := models.GetPostByID(postID)
@@ -165,14 +147,12 @@ func ViewPostHandler(w http.ResponseWriter, r *http.Request) {
 		RenderTemplate(w, "404", nil)      // Render custom 404 page
 		return
 	}
-
 	comments, err := models.GetCommentsByPostID(postID)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError) // Set the 500 status code
 		RenderTemplate(w, "500", nil)                 // Render custom 500 page
 		return
 	}
-
 	data := struct {
 		Post     *models.Post
 		Comments []models.Comment
@@ -180,32 +160,25 @@ func ViewPostHandler(w http.ResponseWriter, r *http.Request) {
 		Post:     post,
 		Comments: comments,
 	}
-
 	RenderTemplate(w, "view_post", data)
 }
-
 func LikeHandler(w http.ResponseWriter, r *http.Request) {
 	postID := r.URL.Query().Get("post_id")
 	like := r.URL.Query().Get("like") // "1" for like, "0" for dislike
-
 	// Logic to update the like/dislike in the database
 	if postID == "" || (like != "1" && like != "0") {
 		http.Error(w, "Invalid like or post ID", http.StatusBadRequest) // 400 Bad Request
 		return
 	}
-
 	log.Printf("Post %s liked: %s", postID, like)
 	http.Redirect(w, r, "/viewPost?id="+postID, http.StatusSeeOther)
 }
-
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	// Destroy the session
 	DestroySession(w, r)
-
 	// Redirect to home page
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
-
 func CategoryHandler(w http.ResponseWriter, r *http.Request) {
-
 }
+
